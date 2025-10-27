ce426ddee16f5e9fddcd82d110656b13d35e02a7
Hamm-code <hamuzahsentongo2@gmail.com>
2025-10-27 22:16:46 +0200

    init

---
+++ car.py
@@ -0,0 +1,14 @@
+class Car:
+    
+    
+
+    def __init__(self,make,yom,color):
+        self.model = make
+        self.yom = yom
+        self.color = color
+
+    def drive(self):
+        print("This car is driving")
+
+    def stop(self):
+        print("This car is stopped")
---
+++ script.py
@@ -0,0 +1,10 @@
+from car import Car
+
+car1 = Car("mercedes", 2021, "Blue")
+
+print(car1.color)
+print(car1.model)
+print(car1.yom)
+
+car1.drive()
+car1.stop()
